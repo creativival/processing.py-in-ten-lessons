7c18209ae3eeaeaf4104e24f8935b897356f3bd9
creativival <creativival@gmail.com>
2021-12-18 17:33:15 +0900

    彩度、明度の理解

--- lesson_1_1.py
+++ lesson_1_1.py
@@ -1,2 +1,13 @@
+if False:
+    from lib.Processing3 import *
+
+
 def setup():
-    size(500,500)
+    colorMode(HSB, 360, 100, 100)
+    noStroke()
+    size(550, 550)
+    for j in range(11):  # 明度（Y軸）
+        for i in range(11):  # 彩度（X軸）
+            fill(360, 10 * i, 10 * j)
+            rect(50 * i, 50 * j, 50, 50)
+    print('hello world')

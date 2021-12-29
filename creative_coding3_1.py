b3af90dfa95e47fbaf4f71c8b1fee5c5f9d4d65e
creativival <creativival@email.com>
2021-12-30 00:43:40 +0900

    P3D 立体を作成

---
+++ creative_coding3_1.py
@@ -0,0 +1,36 @@
+"""
+三角柱
+"""
+if False:
+    from lib.Processing3 import *
+import p3d_util
+
+SIZE_X = 1200
+SIZE_Y = 750
+RADIUS = 300
+HEIGHT = 100
+NUMBER_OF_VERTICES = 3
+
+
+def setup():
+    size(SIZE_X, SIZE_Y, P3D)
+
+
+def draw():
+    background(255)
+    pushMatrix()
+    fill(255, 0, 0, 127)
+    translate(width / 2, height / 4)
+    rotateY(frameCount * 0.01)
+    # box(150, 150, 150)
+    cylinder = p3d_util.Cylinder(RADIUS, HEIGHT, NUMBER_OF_VERTICES, 0)
+    cylinder.draw()
+    popMatrix()
+    pushMatrix()
+    fill(0, 255, 0, 127)
+    translate(width / 2, height * 3/ 4)
+    rotateY(frameCount * 0.01)
+    # box(150, 150, 150)
+    cylinder = p3d_util.Cylinder(RADIUS, HEIGHT, NUMBER_OF_VERTICES, 1)
+    cylinder.draw()
+    popMatrix()
---
+++ p3d_util.py
@@ -0,0 +1,49 @@
+"""
+P3D Util
+"""
+if False:
+    from lib.Processing3 import *
+
+
+class Cylinder:
+    def __init__(self, _radius, _height, _number_of_vertices, _is_corn=0):
+        self.radius = _radius
+        self.height = _height
+        self.number_of_vertices = _number_of_vertices
+        self.is_corn = _is_corn
+
+    def draw(self):
+        angle = TWO_PI / self.number_of_vertices
+        if self.is_corn:
+            beginShape(TRIANGLE_STRIP)
+        else:
+            beginShape(QUADS)
+        for i in range(self.number_of_vertices):
+            vertex(
+                self.radius * sin(angle * i),
+                -self.height/2,
+                self.radius * cos(angle * i),
+            )
+            vertex(
+                self.radius * sin(angle * (i + 1)),
+                -self.height/2,
+                self.radius * cos(angle * (i + 1)),
+            )
+            if self.is_corn:
+                vertex(
+                    0,
+                    self.height/2,
+                    0,
+                )
+            else:
+                vertex(
+                    self.radius * sin(angle * (i + 1)),
+                    self.height/2,
+                    self.radius * cos(angle * (i + 1)),
+                )
+                vertex(
+                    self.radius * sin(angle * i),
+                    self.height/2,
+                    self.radius * cos(angle * i),
+                )
+        endShape()

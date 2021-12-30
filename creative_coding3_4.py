5f1af83f0b8c4dca81c050a24c83edf81b7420e8
creativival <creativival@gmail.com>
2021-12-30 19:49:15 +0900

    円錐を描く

---
+++ creative_coding3_4.py
@@ -0,0 +1,38 @@
+"""
+円柱
+"""
+if False:
+    from lib.Processing3 import *
+import p3d_util
+
+SIZE_X = 1200
+SIZE_Y = 750
+RADIUS = 300
+HEIGHT = 100
+NUMBER_OF_VERTICES = 64  # 64以上は円柱に変換
+
+
+def setup():
+    size(SIZE_X, SIZE_Y, P3D)
+
+
+def draw():
+    background(255)
+    # 錐
+    pushMatrix()
+    fill(0, 255, 0)
+    translate(width / 2, height / 3)
+    rotateX(frameCount * 0.01)
+    # box(150, 150, 150)
+    cylinder = p3d_util.Cylinder(RADIUS, HEIGHT, NUMBER_OF_VERTICES, 1)
+    cylinder.draw()
+    popMatrix()
+    # 柱
+    pushMatrix()
+    fill(255, 0, 0)
+    translate(width / 2, height * 3 / 4)
+    rotateX(frameCount * 0.01)
+    # box(150, 150, 150)
+    cylinder = p3d_util.Cylinder(RADIUS, HEIGHT, NUMBER_OF_VERTICES, 0)
+    cylinder.draw()
+    popMatrix()
--- p3d_util.py
+++ p3d_util.py
@@ -9,6 +9,8 @@ if False:
 
 class Cylinder:
     def __init__(self, _radius, _height, _number_of_vertices, _is_corn=0):
+        if _number_of_vertices >= 64:
+            _number_of_vertices = 64
         self.radius = _radius
         self.height = _height
         self.number_of_vertices = _number_of_vertices
@@ -18,37 +20,55 @@ class Cylinder:
         angle = TWO_PI / self.number_of_vertices
 
         # 底面
-        noStroke()
-        beginShape(TRIANGLE_STRIP)
-        for i in range(self.number_of_vertices):
-            vertex(0, -self.height / 2, 0)
-            vertex(
-                self.radius * sin(angle * i),
-                -self.height / 2,
-                self.radius * cos(angle * i),
-            )
-            vertex(
-                self.radius * sin(angle * (i + 1)),
-                -self.height / 2,
-                self.radius * cos(angle * (i + 1)),
-            )
-            if not self.is_corn:
-                vertex(0, self.height / 2, 0)
+        if self.number_of_vertices >= 64:
+            # 円錐
+            stroke(0)
+            pushMatrix()
+            rotateX(-PI / 2)
+            translate(0, 0, -self.height / 2)
+            ellipse(0, 0, self.radius * 2, self.radius * 2)
+            translate(0, 0, self.height)
+            if self.is_corn:
+                ellipse(0, 0, 1, 1)  # 頂点
+            else:
+                ellipse(0, 0, self.radius * 2, self.radius * 2)
+            popMatrix()
+        else:
+            noStroke()
+            beginShape(TRIANGLE_STRIP)
+            for i in range(self.number_of_vertices):
+                vertex(0, -self.height / 2, 0)
                 vertex(
                     self.radius * sin(angle * i),
-                    self.height / 2,
+                    -self.height / 2,
                     self.radius * cos(angle * i),
                 )
                 vertex(
                     self.radius * sin(angle * (i + 1)),
-                    self.height / 2,
+                    -self.height / 2,
                     self.radius * cos(angle * (i + 1)),
                 )
-
-        endShape()
+                if not self.is_corn:
+                    vertex(0, self.height / 2, 0)
+                    vertex(
+                        self.radius * sin(angle * i),
+                        self.height / 2,
+                        self.radius * cos(angle * i),
+                    )
+                    vertex(
+                        self.radius * sin(angle * (i + 1)),
+                        self.height / 2,
+                        self.radius * cos(angle * (i + 1)),
+                    )
+            endShape()
 
         # 側面
-        stroke(0)
+        if self.number_of_vertices >= 64:
+            # 円錐
+            noStroke()
+        else:
+            stroke(0)
+
         if self.is_corn:
             beginShape(TRIANGLE_STRIP)
         else:
